6440c33b8f1593591d416f1fe3a83181933f091a
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2025-02-28 10:50:14 -0300

    refactor: simplify useState

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,23 +5,17 @@ import Button from "../components/button/index";
 import Input from "../components/input";
 
 function Index() {
-  const [name, setName] = useState<string>(""); // Tipo explícitamente declarado
-  const [name1, setName1] = useState(""); // Inferindo a tipagem
+  const [name, setName] = useState("usuário");
 
   function handleSubmit() {
     const name = "Thiago";
     alert(`Olá, ${name}`);
   }
 
-  function onChangeText(text: string) {
-    console.log(text);
-    setName(text);
-  }
-
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Olá, {name}</Text>
-      <Input label="Nome" onChangeText={(text) => onChangeText(text)} />
+      <Input label="Nome" onChangeText={setName} />
       <Input label="Idade" />
       <Button label={"Enviar"} onPress={handleSubmit} activeOpacity={1} />
       <Button label={"Continuar"} onPress={handleSubmit} />
